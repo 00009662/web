9abf594e305ff10c59941f7d39fa46facf9343fb
00009662 <73440960+00009662@users.noreply.github.com>
2020-12-09 23:42:57 +0500

    final works

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide,2000);
+var slides = document.querySelectorAll('#slides .slide'); //grab from index.html selectors with proper class
+var currentSlide = 0; //slides count
+var slideInterval = setInterval(nextSlide,2000); //auto slider interval
  
-function nextSlide() {
+function nextSlide() { //function which will in every 2000tiks grab new elemet with class slide and give them new class showing to make it visiable via css
     slides[currentSlide].className = 'slide';
     currentSlide = (currentSlide+1)%slides.length;
     slides[currentSlide].className = 'slide showing';
